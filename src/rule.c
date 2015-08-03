regit/nftables

commit e4d21958c8351b8282a3a079bb934c486cd1e0f6

rule: add do_list_tables()
Wrap code to list existing tables in a function.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/rule.c
@@ -900,6 +900,21 @@ static int do_list_ruleset(struct netlink_ctx *ctx, struct cmd *cmd)
 	return 0;
 }
 
+static int do_list_tables(struct netlink_ctx *ctx, struct cmd *cmd)
+{
+	struct table *table;
+
+	if (netlink_list_tables(ctx, &cmd->handle, &cmd->location) < 0)
+		return -1;
+
+	list_for_each_entry(table, &table_list, list)
+		printf("table %s %s\n",
+		       family2str(table->handle.family),
+		       table->handle.table);
+
+	return 0;
+}
+
 static int do_command_list(struct netlink_ctx *ctx, struct cmd *cmd)
 {
 	struct table *table = NULL;
@@ -917,21 +932,8 @@ static int do_command_list(struct netlink_ctx *ctx, struct cmd *cmd)
 
 	switch (cmd->obj) {
 	case CMD_OBJ_TABLE:
-		if (!cmd->handle.table) {
-			/* List all existing tables */
-			struct table *table;
-
-			if (netlink_list_tables(ctx, &cmd->handle,
-						&cmd->location) < 0)
-				return -1;
-
-			list_for_each_entry(table, &ctx->list, list) {
-				printf("table %s %s\n",
-				       family2str(table->handle.family),
-				       table->handle.table);
-			}
-			return 0;
-		}
+		if (!cmd->handle.table)
+			return do_list_tables(ctx, cmd);
 		return do_list_table(ctx, cmd, table);
 	case CMD_OBJ_CHAIN:
 		return do_list_table(ctx, cmd, table);

commit b4100ba0a86b0f8ed5cfc6af9b754e2e97e50b69

src: restore nft list tables
Iterate over the ctx->list which is where the table objects are after
calling netlink_list_tables().

Fixes: e4d21958c835 ("rule: add do_list_tables()")
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/rule.c
@@ -907,7 +907,7 @@ static int do_list_tables(struct netlink_ctx *ctx, struct cmd *cmd)
 	if (netlink_list_tables(ctx, &cmd->handle, &cmd->location) < 0)
 		return -1;
 
-	list_for_each_entry(table, &table_list, list)
+	list_for_each_entry(table, &ctx->list, list)
 		printf("table %s %s\n",
 		       family2str(table->handle.family),
 		       table->handle.table);