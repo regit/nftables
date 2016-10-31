regit/nftables

commit 0423caa91ad26e7a4a6d840566d25d48d3341fa1

src: don't need keyword for log level
We can handle log levels without keywords in our grammar, use string
instead.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/parser_bison.y
@@ -1517,14 +1517,30 @@ log_arg			:	PREFIX			string
 			}
 			;
 
-level_type		:	LEVEL_EMERG	{ $$ = LOG_EMERG; }
-			|	LEVEL_ALERT	{ $$ = LOG_ALERT; }
-			|	LEVEL_CRIT	{ $$ = LOG_CRIT; }
-			|	LEVEL_ERR	{ $$ = LOG_ERR; }
-			|	LEVEL_WARN	{ $$ = LOG_WARNING; }
-			|	LEVEL_NOTICE	{ $$ = LOG_NOTICE; }
-			|	LEVEL_INFO	{ $$ = LOG_INFO; }
-			|	LEVEL_DEBUG	{ $$ = LOG_DEBUG; }
+level_type		:	string
+			{
+				if (!strcmp("emerg", $1))
+					$$ = LOG_EMERG;
+				else if (!strcmp("alert", $1))
+					$$ = LOG_ALERT;
+				else if (!strcmp("crit", $1))
+					$$ = LOG_CRIT;
+				else if (!strcmp("err", $1))
+					$$ = LOG_ERR;
+				else if (!strcmp("warning", $1))
+					$$ = LOG_WARNING;
+				else if (!strcmp("notice", $1))
+					$$ = LOG_NOTICE;
+				else if (!strcmp("info", $1))
+					$$ = LOG_INFO;
+				else if (!strcmp("debug", $1))
+					$$ = LOG_DEBUG;
+				else {
+					erec_queue(error(&@1, "invalid log level", $1),
+						   state->msgs);
+					YYERROR;
+				}
+			}
 			;
 
 limit_stmt		:	LIMIT	RATE	limit_mode	NUM	SLASH	time_unit	limit_burst

src/scanner.l
@@ -299,14 +299,6 @@ addrstring	({macaddr}|{ip4addr}|{ip6addr})
 "snaplen"		{ return SNAPLEN; }
 "queue-threshold"	{ return QUEUE_THRESHOLD; }
 "level"			{ return LEVEL; }
-"emerg"			{ return LEVEL_EMERG; }
-"alert"			{ return LEVEL_ALERT; }
-"crit"			{ return LEVEL_CRIT; }
-"err"			{ return LEVEL_ERR; }
-"warn"			{ return LEVEL_WARN; }
-"notice"		{ return LEVEL_NOTICE; }
-"info"			{ return LEVEL_INFO; }
-"debug"			{ return LEVEL_DEBUG; }
 
 "queue"			{ return QUEUE;}
 "num"			{ return QUEUENUM;}

commit 740b719aa77a2100076d16c58bc5d6e04f731b9f

log: rename the log level "warning" to "warn"
This is to keep compatibility. The original keyword in grammer is
"warn" instead of "warning".

Fixes: 0423caa91ad2 ("src: don't need keyword for log level")
Signed-off-by: Liping Zhang <zlpnobody@gmail.com>
Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

src/parser_bison.y
@@ -1540,7 +1540,7 @@ level_type		:	string
 					$$ = LOG_CRIT;
 				else if (!strcmp("err", $1))
 					$$ = LOG_ERR;
-				else if (!strcmp("warning", $1))
+				else if (!strcmp("warn", $1))
 					$$ = LOG_WARNING;
 				else if (!strcmp("notice", $1))
 					$$ = LOG_NOTICE;