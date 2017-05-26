61428af7486defec6adafc9b6a2ee0602fd98b48
Pablo Neira Ayuso <pablo@netfilter.org>
2017-05-26 11:49:19 +0100

    netlink: add size description for constant sets
    The kernel side can make better decisions with this information when
    selecting the right backend, so add this information to the set netlink
    message.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/netlink.c
+++ src/netlink.c
@@ -1293,6 +1293,8 @@ static int netlink_add_set_batch(struct netlink_ctx *ctx,
 		if (set->desc.size != 0)
 			nftnl_set_set_u32(nls, NFTNL_SET_DESC_SIZE,
 					  set->desc.size);
+	} else if (set->init) {
+		nftnl_set_set_u32(nls, NFTNL_SET_DESC_SIZE, set->init->size);
 	}
 
 	udbuf = nftnl_udata_buf_alloc(NFT_USERDATA_MAXLEN);
