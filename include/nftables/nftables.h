d833fe46af9bab11aa535b727fb3ab70e1f9fdda
Eric Leblond <eric@regit.org>
2017-08-13 23:13:54 +0200

    libnftables: add nft_run_command_from_buffer

--- include/nftables/nftables.h
+++ include/nftables/nftables.h
@@ -20,4 +20,7 @@ void nft_global_deinit(void);
 struct nft_ctx *nft_context_new(void);
 void nft_context_free(struct nft_ctx *nft);
 
+int nft_run_command_from_buffer(struct nft_ctx *nft, struct nft_cache *cache,
+				char *buf, size_t buflen);
+
 #endif
