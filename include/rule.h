7421aec47f81d3e4180648bf4ce84a8d49ea85cb
Pablo Neira Ayuso <pablo@netfilter.org>
2017-08-15 18:45:00 +0200

    rule: remove sequence number from struct eval_ctx
    This field is unused, remove it.
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/rule.h
+++ include/rule.h
@@ -470,7 +470,6 @@ extern void cmd_free(struct cmd *cmd);
  * @set:	current set
  * @stmt:	current statement
  * @cache:	cache context
- * @seqnum:	netlink sequence number
  * @ectx:	expression context
  * @pctx:	payload context
  */
@@ -483,7 +482,6 @@ struct eval_ctx {
 	struct set		*set;
 	struct stmt		*stmt;
 	struct nft_cache	*cache;
-	uint32_t		seqnum;
 	struct expr_ctx		ectx;
 	struct proto_ctx	pctx;
 };
